903fda530927b732686f83d58ac3ea735361749f
Lauren Hamel <lauren.hamel@outlook.com>
2017-09-27 08:54:04 -0400

    Broke into partials
    Broke out the email template into partials, and built initial Sass structure.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,23 +7,95 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     
     // Task configuration.
+    sass: {
+      dev: {
+        options: {
+          sourcemap: 'none',
+          style: 'expanded',
+          noCache: true,
+          update: true
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'src/scss',
+            src: [ '*.scss' ],
+            dest: 'dist/css/',
+            ext: '.css'
+          }
+        ]
+      },
+      dist: {
+        options: {
+          sourcemap: 'none',
+          style: 'compressed',
+          noCache: true
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'src/scss',
+            src: [ '*.scss' ],
+            dest: 'dist/css/',
+            ext: '.css'
+          }
+        ]
+      }
+    },
+    includes: {
+      dist: {
+        options: {
+          includePath: [ 'src/partials/' ]
+        },
+        src: [ '*.html' ],
+        cwd: 'src',
+        dest: 'dist'
+      }
+    },
+    assets_inline: {
+      dist: {
+        files: {
+          'dist/3-column-template.html' : 'dist/3-column-template.html'
+        }
+      }
+    },
     watch: {
-      gruntfile: {
+      options: {
+        atBegin: true
+      },
+      dev: {
+        files: [ 'src/scss/**/*.scss', 'src/**/*.html' ],
+        tasks: [ 'includes:dist', 'sass:dev', 'assets_inline:dist', 'clean:css' ]
+      },
+      config: {
         options: {
           reload: true
         },
-        files: 'Gruntfile.js',
+        files: [ 'Gruntfile.js', 'package.json' ],
         tasks: []
       }
+    },
+    clean: {
+      css: ['dist/css']
     }
     
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-assets-inline');
+  grunt.loadNpmTasks('grunt-includes');
 
   // Default task.
-  grunt.registerTask('default', [ 'dev' ]);
-  grunt.registerTask('dev', [ 'watch' ]);
+  grunt.registerTask('default', ['dev']);
+  grunt.registerTask('dev', ['watch']);
+  grunt.registerTask('dist', [
+    'includes:dist',
+    'sass:dist', 
+    'assets_inline:dist',
+    'clean:css'
+  ]);
 
 };
